1bcc933c0e9e1368ff229bb2288203aec6d36bb9
julietmasica <71616199+julietmasica@users.noreply.github.com>
2021-05-12 08:23:45 -0400

    idk

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const query = async function () {
   try {
-    const response = await fetch("https://api.fbi.gov/wanted/v1/list");
+    const response = await fetch(
+      "cors-anywhere.herokuapp.com/https://api.fbi.gov/wanted/v1/list"
+    );
     const data = await response.json();
     console.log(data);
   } catch (error) {}
