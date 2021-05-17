b6f315f7e8634193d91c7416b18ba00a1ca7b7ae
dieuanhn <71412021+dieuanhn@users.noreply.github.com>
2021-05-17 18:07:26 -0400

    not so good lol

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const query = async function () {
   try {
     const response = await fetch(
-      "https://cors-anywhere.herokuapp.com/api.fbi.gov/wanted/v1/list"
+      "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?age-group=16&api-key=IjOw569tP2k0fNoFgXAAiEG4R2g1C3fm"
     );
     const data = await response.json();
     console.log(data);
   } catch (error) {
     console.log(error);
+    alert("hey something went wrong");
   }
 };
 query();
