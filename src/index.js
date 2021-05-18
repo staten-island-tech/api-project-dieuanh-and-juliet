9da23aad0de0f1ddf09a2aaddd289400100a064d
dieuanhn <71412021+dieuanhn@users.noreply.github.com>
2021-05-17 20:47:54 -0400

    djdbbw

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ const query = async function () {
     const response = await fetch(
       "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?age-group=16&api-key=IjOw569tP2k0fNoFgXAAiEG4R2g1C3fm"
     );
-    const data = await response.json();
-    data.results.forEach((book) => {
-      DOMSelectors.grid.insertAdjacentHTML("beforeend", `<div class="book-cover">
-      <img src="" alt="" class="cover">
-  </div>
-  <div class="book-info">
-      <h3 class="name">${book.title}</h3>
-      <h4 class="author">${book.author}</h4>
-      <h4 class="description">${book.description}</h4>`);
+    //   const data = await response.json();
+    //   data.results.forEach((book) => {
+    //     DOMSelectors.grid.insertAdjacentHTML("beforeend", `<div class="book-cover">
+    //     <img src="" alt="" class="cover">
+    // </div>
+    // <div class="book-info">
+    //     <h3 class="name">${book.title}</h3>
+    //     <h4 class="author">${book.author}</h4>
+    //     <h4 class="description">${book.description}</h4>`);
   } catch (error) {
     console.log(error);
     alert("hey something went wrong");
