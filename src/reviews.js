ae3123cd45a6233dcec6b3aae95586c6c6000a27
dieuanhn <71412021+dieuanhn@users.noreply.github.com>
2021-05-24 17:35:45 -0400

    ???

--- src/reviews.js
+++ src/reviews.js
@@ -1,8 +1,9 @@
-import { async } from "q";
 import { DOMSelectors } from "./DOM";
 
 const listen = function () {
   DOMSelectors.searchForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    DOMSelectors.grid.innerHTML = "";
     const searchParams = DOMSelectors.searchArea.value;
     const searchQuery = async function () {
       try {
@@ -18,9 +19,9 @@ const listen = function () {
           DOMSelectors.grid.insertAdjacentHTML(
             "beforeend",
             `<div class="book-info">
-            <h3 class="name">${book.title}</h3>
-            <h4 class="author">Written By: ${book.author}</h4>
-            <h4 class="description">Description: ${book.description}</h4>`
+            <h3 class="name">${book.book_title}</h3>
+            <h4 class="author">Written By: ${book.book_author}</h4>
+            <h4 class="description">Description: ${book.summary}</h4>`
           );
         });
       } catch (error) {
